bf583903628e056cbaa3b3e4d6fe57ca619cf726
Prince Ahmed <pa99@duke.edu>
2023-08-14 01:17:14 +0100

    added gpa and scholarship

--- assets/js/main.js
+++ assets/js/main.js
@@ -257,7 +257,7 @@
   /**
    * Initiate Pure Counter 
    */
-  new PureCounter();
+  // new PureCounter();
 
   const hero = document.getElementById('hero');
   const images = [
@@ -295,6 +295,27 @@
 
   // Change background image every 5 seconds
   setInterval(changeBackgroundImage, 5000);
+
+  new PureCounter({
+    // Setting that can't' be overriden on pre-element
+    selector: ".purecounter", // HTML query selector for spesific element
+  
+    // Settings that can be overridden on per-element basis, by `data-purecounter-*` attributes:
+    start: 0, // Starting number [uint]
+    end: 3.959, // End number [uint]
+    duration: 1, // The time in seconds for the animation to complete [seconds]
+    delay: 10, // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+    once: true, // Counting at once or recount when the element in view [boolean]
+    pulse: false, // Repeat count for certain time [boolean:false|seconds]
+    decimals: 3, // How many decimal places to show. [uint]
+    legacy: true, // If this is true it will use the scroll event listener on browsers
+    filesizing: false, // This will enable/disable File Size format [boolean]
+    currency: false, // This will enable/disable Currency format. Use it for set the symbol too [boolean|char|string]
+    formater: "us-US", // Number toLocaleString locale/formater, by default is "en-US" [string|boolean:false]
+    separator: true // This will enable/disable comma separator for thousands. Use it for set the symbol too [boolean|char|string]
+  });
+  
+
 
 
 })()
