7ce53601166e4fb77de3b276adde9d7ccbc545d4
Prince Ahmed <pa99@duke.edu>
2023-08-14 05:06:51 +0100

    loading page

--- assets/js/main.js
+++ assets/js/main.js
@@ -318,6 +318,10 @@
   });
   
 
+  window.addEventListener("load", function () {
+    document.getElementById("preloader").style.display = "none";
+    document.getElementById("content").style.display = "block";
+  });
 
 
 })()
